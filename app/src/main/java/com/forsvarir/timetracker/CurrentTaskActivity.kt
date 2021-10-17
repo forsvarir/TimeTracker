524c07502271a4315972826fd7c470c1a9c00df9
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-10-17 23:27:49 +0100

    Started adding activity for tracking current task

--- app/src/main/java/com/forsvarir/timetracker/CurrentTaskActivity.kt
+++ app/src/main/java/com/forsvarir/timetracker/CurrentTaskActivity.kt
@@ -1,46 +1,58 @@
 package com.forsvarir.timetracker
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.forsvarir.timetracker.ui.theme.TimeTrackerTheme
 
-class MainActivity : ComponentActivity() {
+class CurrentTaskActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TimeTrackerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("TimeTracker")
+                    TaskView()
                 }
             }
         }
-        startCurrentTaskActivity()
-    }
-
-    fun startCurrentTaskActivity() {
-        val intent = Intent(this, CurrentTaskActivity::class.java).apply {
-        }
-        startActivity(intent)
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskView() {
+    Column {
+        Text(
+            text = "Time Tracker",
+            style = MaterialTheme.typography.subtitle1
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row {
+            Text(
+                text = "Programming",
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "00:11",
+                style = MaterialTheme.typography.subtitle2
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreview2() {
     TimeTrackerTheme {
-        Greeting("TimeTracker")
+        TaskView()
     }
 }
