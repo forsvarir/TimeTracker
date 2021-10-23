15e1f1b3e14d5f618eaa19c245f07e311aaa3b5f
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-10-23 13:12:28 +0100

    Improved selection of current activity

--- app/src/main/java/com/forsvarir/timetracker/MainActivity.kt
+++ app/src/main/java/com/forsvarir/timetracker/MainActivity.kt
@@ -3,11 +3,16 @@ package com.forsvarir.timetracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.forsvarir.timetracker.ui.theme.TimeTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +30,11 @@ fun CurrentTaskScreen() {
     TimeTrackerTheme {
         Scaffold(topBar = {
             TopAppBar()
-        }) {
-            Surface(color = MaterialTheme.colors.background) {
+        }) { innerPadding ->
+            Surface(
+                color = MaterialTheme.colors.background,
+                modifier = Modifier.padding(innerPadding)
+            ) {
                 TaskView()
             }
         }
@@ -45,11 +53,26 @@ private fun TopAppBar() {
 
 @Composable
 fun TaskView() {
-    Column {
+    var update by remember { mutableStateOf(false) }
+    var currentItem by remember { mutableStateOf("Programming") }
+
+    Column(Modifier.padding(all = 8.dp)) {
         Row(horizontalArrangement = Arrangement.Center) {
             Text(text = "Current Activity", style = MaterialTheme.typography.h3)
         }
-        EventRow("Programming", "00.11")
+        Box(
+            modifier = Modifier
+                .wrapContentSize(Alignment.TopStart)
+        ) {
+            if (!update) {
+                CurrentActivityStatus(currentItem) { update = true }
+            } else {
+                SelectCurrentActivity {
+                    currentItem = it
+                    update = false
+                }
+            }
+        }
         Row(horizontalArrangement = Arrangement.Center) {
             Text(text = "Previous Events", style = MaterialTheme.typography.h3)
         }
@@ -69,6 +92,65 @@ fun TaskView() {
         EventRow("Walking", "01.11")
         EventRow("Sleeping", "08.01")
         EventRow("Eating", "02.11")
+    }
+}
+
+@Composable
+fun SelectCurrentActivity(onSelected: (newItem: String) -> Unit) {
+    var selectedIndex by remember { mutableStateOf(0) }
+    val items = listOf("Programming", "Walking", "Sleeping", "Eating")
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopStart)
+    ) {
+        Text(
+            items[selectedIndex],
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(onClick = { expanded = true })
+                .background(
+                    Color.Gray
+                )
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+        ) {
+            items.forEachIndexed { index, itemText ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    selectedIndex = index
+                    onSelected(itemText)
+                }) {
+                    Text(itemText)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CurrentActivityStatus(currentActivity: String, onUpdate: () -> Unit) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = currentActivity,
+            style = MaterialTheme.typography.subtitle2
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = "TickTickTick",
+            style = MaterialTheme.typography.subtitle2
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        OutlinedButton(onClick = { onUpdate() }) {
+            Text("ChangeTask")
+        }
     }
 }
 
