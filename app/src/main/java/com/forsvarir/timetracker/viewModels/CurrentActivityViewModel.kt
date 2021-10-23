18387d8df83f41ed5113d1434b9bd880679c80cc
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-10-23 16:11:44 +0100

    Migrate unit test to assertJ

---
+++ app/src/main/java/com/forsvarir/timetracker/viewModels/CurrentActivityViewModel.kt
@@ -0,0 +1,5 @@
+package com.forsvarir.timetracker.viewModels
+
+class CurrentActivityViewModel {
+
+}
---
+++ app/src/test/java/com/forsvarir/timetracker/CurrentActivityTests.kt
@@ -0,0 +1,11 @@
+package com.forsvarir.timetracker
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class CurrentActivityTests {
+    @Test
+    fun activitiesAvailable() {
+        assertThat(2 + 2).isEqualTo(4)
+    }
+}
