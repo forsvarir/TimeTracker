92e20878e63edb89247d3205a23bacbadad0a237
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-11-04 23:50:43 +0000

    Added commands for changing orientation during test

--- app/src/androidTest/java/com/forsvarir/timetracker/CurrentActivityTransitionTests.kt
+++ app/src/androidTest/java/com/forsvarir/timetracker/CurrentActivityTransitionTests.kt
@@ -1,10 +1,10 @@
 package com.forsvarir.timetracker
 
-import android.content.pm.ActivityInfo
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.forsvarir.timetracker.control.CurrentActivityScreenController
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,12 +15,18 @@ class CurrentActivityTransitionTests {
     @Test
     fun selectingNewActivityChanges() {
         launchCurrentActivityScreen(mainActivityRule) {
-            mainActivityRule.activity.requestedOrientation =
-                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-
             setCurrentActivity("Programming")
+        } verify {
+            currentActivityIs("Programming")
+        }
+    }
 
-
+    @Test
+    @Ignore("State not saved correctly yet")
+    fun currentActivityPersistsThroughOrientationChange() {
+        launchCurrentActivityScreen(mainActivityRule) {
+            setCurrentActivity("Programming")
+            orientateLandscape()
         } verify {
             currentActivityIs("Programming")
         }
--- app/src/androidTest/java/com/forsvarir/timetracker/control/CurrentActivityScreenController.kt
+++ app/src/androidTest/java/com/forsvarir/timetracker/control/CurrentActivityScreenController.kt
@@ -1,5 +1,7 @@
 package com.forsvarir.timetracker.control
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
@@ -8,6 +10,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.forsvarir.timetracker.MainActivity
 import com.forsvarir.timetracker.R
 import com.forsvarir.timetracker.verification.CurrentActivityScreenVerifier
+import java.lang.Thread.sleep
 
 class CurrentActivityScreenController(private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>) {
     fun navigateToActivityHistoryScreen() {
@@ -27,6 +30,28 @@ class CurrentActivityScreenController(private val rule: AndroidComposeTestRule<A
         rule
             .onNodeWithText(newActivity)
             .performClick()
+    }
+
+    fun orientatePortrait() {
+        setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, Configuration.ORIENTATION_PORTRAIT)
+    }
+
+    fun orientateLandscape() {
+        setOrientation(
+            ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE,
+            Configuration.ORIENTATION_LANDSCAPE
+        )
+    }
+
+    private fun setOrientation(to: Int, expectedTo: Int) {
+        rule.activity.requestedOrientation = to
+
+        for (i in 1..5) {
+            if (rule.activity.resources.configuration.orientation == expectedTo) {
+                break
+            }
+            sleep(50)
+        }
     }
 
     infix fun verify(
